d43477212f2f93ebb4d34a9d942e9d0035948e0c
oriolclosa <oriol.closa@est.fib.upc.edu>
2019-03-08 21:23:19 +0100

    Initial Django site

---
+++ website/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/oriolclosa/Documents/STARTHack2019/starthack2019/website/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
