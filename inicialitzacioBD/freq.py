6e82dea264dfdccbcaf84016308eb9347b4eaeb4
Jaume <jaume.pladevall@est.fib.upc.edu>
2019-03-09 02:57:31 +0100

    Improve approximation of passengers

--- inicialitzacioBD/freq.py
+++ inicialitzacioBD/freq.py
@@ -5,18 +5,19 @@ if __name__ == '__main__':
 	with open('passagierfrequenz.json') as json_data:
 		d = json.load(json_data)
 		res = {}
-		anteriors_passatgers = 500
+		reduction = 0.25
+		last_travelers = 200
 		for row in d:
 			try:
-				estacio = row['fields']['bahnhof_haltestelle']
-				passatgers = row['fields']['dtv']
+				station = row['fields']['bahnhof_haltestelle']
+				travelers = row['fields']['dtv']
 			except KeyError:  # includes simplejson.decoder.JSONDecodeError
-				passatgers = -1
-			if passatgers != -1:
-				res[str(estacio)] = passatgers
-				anteriors_passatgers = passatgers
+				travelers = -1
+			if travelers != -1:
+				res[str(station)] = int(travelers*reduction)
+				last_travelers = travelers
 			else:
-				res[str(estacio)] = anteriors_passatgers
+				res[str(station)] = int(last_travelers)
 
 		print(res)
 	with open('result.json', 'w') as outfile:
