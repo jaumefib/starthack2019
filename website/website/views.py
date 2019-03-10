5c36d356244b83de8cb3314e48d8941dea733b56
David Martínez <dmartinezmillan@hotmail.com>
2019-03-10 04:37:43 +0100

    aa

--- website/website/views.py
+++ website/website/views.py
@@ -233,9 +233,9 @@ class Scan(TabsView):
 
         try:
             if user.role == 4:
-                cup = models.Cup.objects.filter(id=qrcode)
+                cup = models.Cup.objects.filter(id=qrcode).first()
                 cup.user.increment_balance()
-                cup.first().return_to_dropoff(user.dropOff)
+                cup.return_to_dropoff(user.dropOff)
                 cup.dropOff.increment_current()
             else:
                 cup = models.Cup.objects.filter(id=qrcode, user=None)
