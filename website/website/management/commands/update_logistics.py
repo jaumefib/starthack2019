6699fcad8cdfebdf18c108548a5be6a85ade8eaf
oriolclosa <oriol.closa@est.fib.upc.edu>
2019-03-10 03:23:10 +0100

    Coez

--- website/website/management/commands/update_logistics.py
+++ website/website/management/commands/update_logistics.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for station in models.Station.objects.all():
+            print("Doing station " + station.name)
             cups_to_clean = 0
             for droppOff in models.DropOff.objects.filter(station=station):
                 cups_to_clean += models.Cup.objects.filter(dropOff=droppOff, user=None).count()
